f11fb739b86cbe5c09e5ea2b7ee30a800d58318a
Michael Constanza <michael.constanza@gmail.com>
2016-09-07 20:34:16 -0400

    added error protection and alert message for duplicate movie entries. Form now clears after user enters input.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -10,17 +10,15 @@ $(document).ready(function(){
 	var $addButton = $('#addButton');
 
 
-
-
-
-	// holds the movie searches that will be made into buttons, including ones the user
-	// inputs
+	// holds the movie searches that will be made into default buttons, including 
+	// ones the user inputs
 	var buttonArray = ["Indiana Jones", "Star Wars", "The Godfather", "Back to the Future",
 	"Jaws", "Close Encounters of the Third Kind", "The Goonies"];
 
 
-	// General Functions ////////////////////////////////////////////////////////////////////
+// General Functions ////////////////////////////////////////////////////////////////////
 
+	// creates buttons from the buttonArray
 	function renderButtons(){
 
 		// empty the button div each time buttons are rendered so that they are not repeated
@@ -45,6 +43,18 @@ $(document).ready(function(){
 		};
 	};
 
+	// checks if movie title is already in the buttonArray
+	function movieTitleValid(movie){
+
+		for( var i = 0; i < buttonArray.length; i++){
+			if(movie.toLowerCase() == buttonArray[i].toLowerCase()){
+				$('#movieExistsAlert').show();
+				return false;
+			};	
+		};
+		return true;
+	};
+
 	// converts an array to a string without commas between elements
 	function arrayToString(array){
 		var string = ""
@@ -52,7 +62,7 @@ $(document).ready(function(){
 			string += array[i];
 		}
 		return string
-};
+	};
 	
 	// change the name contained in the button to a string usable in the API query
 	function makeMovieQuery(movieTitle){
@@ -67,50 +77,63 @@ $(document).ready(function(){
 		};
 
 		return arrayToString(movie);
-	}
+	};
 
+	// takes an array of gifs (or a jQuery selector of all gifs) and stops them from playing
 	function stopGifs(gifs){
 		
 		for(var i = 0; i < gifs.length; i++){
-		
-			console.log(gifs[i].src);
 
 			$(gifs[i]).attr('src', $(gifs[i]).data('gifStill'));
 		}
-	}
+	};
 
 // On click functions /////////////////////////////////////////////////////////////////////
 
 
-	// function for when the user clicks the "Get Gifs!" button for the form in the navbar
+	// function for when the user clicks the "Add Movie!" button for the form in the navbar
 	$addButton.on('click', function(){
+
+		// hide the error message if it is shown
+		$('#movieExistsAlert').hide();
 
 		// Take input from text box and add to button array for rendering
 		var movie = $movieInput.val().trim();
-		console.log($movieInput.val())
 
-		buttonArray.push(movie);
+		// verify user input before pushing to array
 
+		if(movieTitleValid(movie)){
+			buttonArray.push(movie);
+		};
+		
 		// render the buttons in the buttonArray
 		renderButtons();
+
+		// clears the form of the user's previous input
+		$movieInput.val('');
 
 		// return false so that users can hit 'enter' without changing pages
 		return false;
 	});
 
+
+	// when the user clicks on an individual movie button
 	$(document).on("click", '.movie', function(){
 
+		// clear the div of all gifs
 		$gifDiv.empty();
 
+		// create the query url 
 		var movie = makeMovieQuery($(this).data('name'));
 		var limit = "&limit=10";
 		var publicKey = "&api_key=dc6zaTOxFJmzC";
 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + publicKey + limit;
 
+		// call the giphy API
 		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
 
-			console.log(response)
 			// response is an array of objects
+			// iterate through the array and assign relevant properties to variables
 
 			for(var i = 0; i < response.data.length; i++){
 
@@ -118,37 +141,49 @@ $(document).ready(function(){
 				var url = response.data[i].images.original.url;
 				var rating = response.data[i].rating;
 
+				// create the images
 				var $gif = $('<img>');
 				$gif.attr('class', 'gif');
-				$gif.data('gif', url);
-				$gif.data('gifStill', stillUrl)
-				$gif.attr('src', stillUrl);
 
+				// save the animated gif url
+				$gif.data('gif', url);
+				// save the still image url
+				$gif.data('gifStill', stillUrl)
+				// set the gif's default src to the still image
+				$gif.attr('src', stillUrl);
+				// add the gif to the gifDiv
 				$gifDiv.append($gif)
 			};	
 		});
 	});
 
+	// when the user click on a gif
 	$(document).on('click', '.gif', function(){
 
-		var $gifs = $('.gif')
+		// create variable for event
+		var $gif = $(this);
+		// create array of all current gifs
+		var $gifs = $('.gif');
 
+		// if a gif is currently playing, stop it
+		if($gif.attr('src') == $gif.data('gif')){
 
-		if($(this).attr('src') == $(this).data('gif')){
-
-			$(this).attr('src', $(this).data('gifStill'))
+			$gif.attr('src', $gif.data('gifStill'))
 
 		}else{
 
+			// stop all gifs
 			stopGifs($gifs);
 
-			$(this).attr('src', $(this).data('gif'))
+			// play the current gif
+			$gif.attr('src', $gif.data('gif'))
 		}
 	});
 
 
-
-
+	// make sure alert is hidden to start
+	$('#movieExistsAlert').hide();
+	
 	// render the default buttons on page load
 	renderButtons();
 
